cf83146c8358188c35ff3b4305becff6923c88b7
krishmodi0202 <krishmodi33@gmail.com>
2025-02-03 10:21:11 +0530

    Updated project files and installed new packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { reportWebVitals } from 'web-vitals';
 
 function App() {
   const [credits, setCredits] = useState('');
